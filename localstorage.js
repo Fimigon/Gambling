e3b8a36b25f647a092052197e8cc75b1444332a7
Fimigon <Lcisija1@gmail.com>
2023-01-10 22:39:18 -0600

    Fixed Login and SignUp

--- localstorage.js
+++ localstorage.js
@@ -1,26 +1,58 @@
-const signupForm = document.getElementById('signup-form');
-const loginForm = document.getElementById('login-form');
+// JavaScript object to store the users
+const users = [];
 
 // Signup form submit
+const signupForm = document.getElementById('signup-form');
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = signupForm.name.value;
     const email = signupForm.email.value;
     const password = signupForm.password.value;
-    
-    // input validation
-    if (!name || !email || !password) {
+    const passwordConfirm = signupForm.password_confirm.value;
+
+    // Input validation
+    if (!name || !email || !password || !passwordConfirm) {
         return alert('All fields are required');
     }
-    
-    // Store user data
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    if (password !== passwordConfirm) {
+        return alert('Passwords do not match');
+    }
+
+    // Check if the email is already in use
+    const existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+        return alert('Email already in use');
+    }
+
+    // Add new user
     const newUser = { name, email, password };
     users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
     alert('User created successfully');
     signupForm.reset();
 });
 
 // Login form submit
-login
+const loginForm = document.getElementById('login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    // Input validation
+    if (!email || !password) {
+        return alert('All fields are required');
+    }
+
+    // Check user credentials
+    const user = users.find(user => user.email === email);
+    if (!user) {
+        return alert('User not found');
+    }
+    if (user.password !== password) {
+        return alert('Incorrect password');
+    }
+
+    // Log user in
+    alert('Welcome ' + user.name);
+    loginForm.reset();
+});
